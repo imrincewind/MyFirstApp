96366aa0ad948b413703c5de86cb29f636defe56
Ezequiel Fazio <ezfazio@gmail.com>
2017-04-16 20:57:39 -0300

    Multiplication and divition added; some improves on elements distribution; new icon

--- app/src/main/java/com/example/eze/myfirstapp/Main2Activity.java
+++ app/src/main/java/com/example/eze/myfirstapp/Main2Activity.java
@@ -14,10 +14,34 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         Intent intent = getIntent();
-        String aMostrar = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        //String aMostrar = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+       // TextView textView = new TextView(this);
+        //textView.setTextSize(40);
+        //textView.setText("La suma es: " +  aMostrar + " si señor.");
+        boolean suma = getIntent().getExtras().getBoolean("suma");
+        boolean resta = getIntent().getExtras().getBoolean("resta");
+        boolean multiplicacion = getIntent().getExtras().getBoolean("multiplicacion");
+        int[] arr = intent.getIntArrayExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = new TextView(this);
         textView.setTextSize(40);
-        textView.setText("La suma es: " +  aMostrar + " si señor.");
+        int ans = 0;
+        String ansb = "Vuelva atrás y seleccione alguna opción";
+        if(resta){
+            ans = arr[0] - arr[1];
+            ansb = "La resta resulta: " +  ans + " si señor.";
+        } else if (suma) {
+            ans = arr[0] + arr[1];
+            ansb = "La suma es: " +  ans + " si señor.";
+        } else if (multiplicacion) {
+            ans = arr[0] * arr[1];
+            ansb = "La multiplicación da: " +  ans + " si señor.";
+        } else{
+            double x = arr[0];
+            double y = arr[1];
+            double d = x / y;
+            ansb = "La división da: " +  d + " si señor.";
+        }
+        textView.setText(ansb);
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_main2);
         layout.addView(textView);
--- app/src/main/java/com/example/eze/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/eze/myfirstapp/MainActivity.java
@@ -4,10 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    protected final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private boolean s = true;
+    private boolean r = false;
+    private boolean m = true;
+    private boolean d = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,15 +21,33 @@ public class MainActivity extends AppCompatActivity {
     }
     public void sendMessage(View view) {
         Intent intent = new Intent(this, Main2Activity.class);
+        RadioButton suma = (RadioButton) findViewById(R.id.suma);
+        RadioButton resta = (RadioButton) findViewById(R.id.resta);
+        RadioButton multiplicacion = (RadioButton) findViewById(R.id.multiplicacion);
+        //RadioButton division = (RadioButton) findViewById(R.id.division);
+        s = suma.isChecked();
+        r = resta.isChecked();
+        m = multiplicacion.isChecked();
+       // d = division.isChecked();
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         EditText editText2 = (EditText) findViewById(R.id.edit_message2);
         String message2 = editText2.getText().toString();
         int a =  Integer.parseInt(message.toString());
         int b =  Integer.parseInt(message2.toString());
-        Integer c = a + b;
+        int[] d = new int[2];
+        d[0] = 0;
+        d[1] = 0;
+        d[0] = a;
+        d[1] = b;
+        //Integer c = a + b;
 
-        intent.putExtra(EXTRA_MESSAGE, c.toString());
+        //intent.putExtra(EXTRA_MESSAGE, c.toString());
+        intent.putExtra("suma",s);
+        intent.putExtra("resta",r);
+        intent.putExtra("multiplicacion",m);
+        //intent.putExtra("division",d);
+        intent.putExtra(EXTRA_MESSAGE, d);
         startActivity(intent);
     }
 }
